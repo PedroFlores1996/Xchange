22caca186fe3a84478db7e6f9077d36ab27e519a
qxz1rq4 <pedro.albuquerque@ctw.bmwgroup.com>
2025-02-05 13:38:08 +0000

    minor formatter changes

--- app/index.py
+++ app/index.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 
-bp = Blueprint('index', __name__)
+bp = Blueprint("index", __name__)
 
-@bp.route('/')
+
+@bp.route("/")
 @login_required
 def home_page():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@bp.route("/home")
+@login_required
+def home():
+    return render_template("home.html")
