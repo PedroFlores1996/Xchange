edcc5163873be515b28c269cf1da7d760519600e
qxz1rq4 <pedro.albuquerque@ctw.bmwgroup.com>
2025-02-05 13:41:18 +0000

    fix migrations

--- migrations/versions/18294e934b96_.py
+++ migrations/versions/18294e934b96_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: ddbf622f0faf
+Revision ID: 18294e934b96
 Revises: 
-Create Date: 2025-02-02 15:43:11.350238
+Create Date: 2025-02-05 13:39:57.762077
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ddbf622f0faf'
+revision = '18294e934b96'
 down_revision = None
 branch_labels = None
 depends_on = None
