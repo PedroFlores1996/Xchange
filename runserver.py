8b8add7327f014d0505947fc8bddac629cc716d8
qxz1rq4 <pedro.albuquerque@ctw.bmwgroup.com>
2025-02-06 22:17:59 +0000

    formatter applied on runserver

--- runserver.py
+++ runserver.py
@@ -1,6 +1,7 @@
 from app import create_app
 from app.config import TestConfig
+from app.database import db
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app(config=TestConfig)
     app.run(debug=True)
