aeddd7737387fa35c53942b7236c29dfc25ada4b
qxz1rq4 <pedro.albuquerque@ctw.bmwgroup.com>
2025-02-09 20:41:12 +0000

    add missing and invalid form tests for login

--- tests/auth/test_login.py
+++ tests/auth/test_login.py
@@ -49,3 +49,26 @@ def test_login_post_invalid_password(db_session, client, reset_login, csrf_token
     assert response.status_code == 302
     assert response.location == url_for("auth.login")
     assert not current_user.is_authenticated
+
+
+def test_login_post_missing_csrf_token(db_session, client, reset_login, captured_templates):
+    response = client.post("/login", data={"username": "user1", "password": "password1"})
+
+    assert response.status_code == 200
+    assert captured_templates[0][0].name == "auth/login.html"
+    assert isinstance(captured_templates[0][1]["form"], LoginForm)
+    assert captured_templates[0][1]["form"].errors == {"csrf_token": ["The CSRF token is missing."]}
+    assert not current_user.is_authenticated
+
+
+def test_login_invalid_form(db_session, client, reset_login, csrf_token, captured_templates):
+    response = client.post("/login", data={"csrf_token": csrf_token})
+
+    assert response.status_code == 200
+    assert captured_templates[0][0].name == "auth/login.html"
+    assert isinstance(captured_templates[0][1]["form"], LoginForm)
+    assert captured_templates[0][1]["form"].errors == {
+        "username": ["This field is required."],
+        "password": ["This field is required."],
+    }
+    assert not current_user.is_authenticated
