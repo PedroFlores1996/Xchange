9b2663b30dbd21ad06980fa2720a81d1ede5518f
PedroFlores1996 <pedro.flores.albuquerque@gmail.com>
2025-02-16 20:50:09 +0000

    update database to allow for declarative mappings

--- app/database.py
+++ app/database.py
@@ -1,14 +1,18 @@
 from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
 from flask_sqlalchemy import SQLAlchemy
 
-naming_convention = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(column_0_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s",
-}
 
-metadata = MetaData(naming_convention=naming_convention)
+class Base(DeclarativeBase):
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(column_0_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
 
-db = SQLAlchemy(metadata=metadata)
+
+db = SQLAlchemy(model_class=Base)
