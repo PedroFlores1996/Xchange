7c228dee30cc563d1d101468d9ee5763957ad1e0
PedroFlores1996 <pedro.flores.albuquerque@gmail.com>
2025-02-20 10:14:59 +0000

    add balances, group and type (category) to expense

--- app/model/expense.py
+++ app/model/expense.py
@@ -4,29 +4,38 @@ from typing import TYPE_CHECKING, Self, List
 if TYPE_CHECKING:
     from app.model.user import User
     from app.model.balance import Balance
+    from app.model.group import Group
 
 from datetime import datetime
 from sqlalchemy.orm import Mapped, relationship
 
 from app.database import db
+from enum import Enum
+
+
+class ExpenseType(Enum):
+    ACCOMMODATION = "accommodation"
+    DRINKS = "drinks"
+    ENTERTAINMENT = "entertainment"
+    FOOD = "food"
+    GAS = "gas"
+    GROCERIES = "groceries"
+    OTHER = "other"
+    TICKETS = "tickets"
+    UTILITIES = "utilities"
 
 
 class Expense(db.Model):  # type: ignore
     id: Mapped[int] = db.mapped_column(primary_key=True)
     amount: Mapped[float] = db.mapped_column(nullable=False)
     description: Mapped[str] = db.mapped_column(nullable=False)
+    type: Mapped[ExpenseType] = db.mapped_column(nullable=True)
+    balances: Mapped[List[Balance]] = relationship()
+    group_id: Mapped[int] = db.mapped_column(db.ForeignKey("group.id"), nullable=False)
+    group: Mapped[Group] = relationship(back_populates="expenses")
     creator_id: Mapped[int] = db.mapped_column(db.ForeignKey("user.id"), nullable=False)
     creator: Mapped[User] = relationship(foreign_keys=[creator_id])
     created_at: Mapped[datetime] = db.mapped_column(default=datetime.now())
-    updated_by_id: Mapped[int] = db.mapped_column(
-        db.ForeignKey("user.id"), nullable=True
-    )
-    updated_by: Mapped[User] = relationship(foreign_keys=[updated_by_id])
+    updater_id: Mapped[int] = db.mapped_column(db.ForeignKey("user.id"), nullable=True)
+    updater: Mapped[User] = relationship(foreign_keys=[updater_id])
     updated_at: Mapped[datetime] = db.mapped_column(onupdate=datetime.now())
-    balances: Mapped[List[Balance]] = relationship(back_populates="expense")
-
-    def create(cls, amount: float, description: str, creator: User) -> Self:
-        new_expense = cls(amount=amount, description=description, creator=creator)
-        db.session.add(new_expense)
-        db.session.commit()
-        return new_expense
