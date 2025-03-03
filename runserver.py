4f49f34de8c80d05dcfc7fb5990e42f9bd85c39f
PedroFlores1996 <pedro.flores.albuquerque@gmail.com>
2025-03-03 07:12:53 +0000

    runserver uses dev config instead of test one

--- runserver.py
+++ runserver.py
@@ -1,7 +1,7 @@
 from app import create_app
-from app.config import TestConfig
+from app.config import Config
 from app.database import db
 
 if __name__ == "__main__":
-    app = create_app(config=TestConfig)
+    app = create_app(config=Config)
     app.run(debug=True)
