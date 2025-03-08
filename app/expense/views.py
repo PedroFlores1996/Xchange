011edcbb5d4e8381dd9c312cbbc58e93c4b6a76c
PedroFlores1996 <pedro.flores.albuquerque@gmail.com>
2025-03-08 16:44:39 +0000

    add expense creation form, debts view and expense creation summary

--- app/expense/views.py
+++ app/expense/views.py
@@ -4,6 +4,8 @@ from flask_login import login_required
 from app.expense.forms import ExpenseForm
 from app.expense.mapper import ExpenseData, expense_data_from
 from app.expense.processor import create_expense_from
+from app.model.expense import Expense
+from app.model.debt import Debt
 
 bp = Blueprint("expense", __name__)
 
@@ -14,11 +16,18 @@ def expenses() -> str | Response:
     form = ExpenseForm()
     if form.validate_on_submit():
         data: ExpenseData = expense_data_from(form)
-        expense = create_expense_from(data)
-        return redirect(url_for("expense.success"))
+        expense: Expense = create_expense_from(data)
+        return render_template("expense/summary.html", expense=expense)
     return render_template("expense/expense.html", form=form)
 
 
 @bp.route("/success")
 def success():
     return "Success"
+
+
+@bp.route("/debts", methods=["GET"])
+@login_required
+def debts() -> str | Response:
+    all_debts = Debt.query.all()
+    return render_template("expense/debts.html", debts=all_debts)
