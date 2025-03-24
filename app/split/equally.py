16d14f6512b58cac8ba7af623c3c0dd38f2a2714
PedroFlores1996 <pedro.flores.albuquerque@gmail.com>
2025-03-24 18:32:50 +0000

    fix split equally type hints

--- app/split/equally.py
+++ app/split/equally.py
@@ -1,2 +1,2 @@
-def split(total_amount: float, users: dict[int, float]) -> dict[int, float]:
+def split(total_amount: float, users: dict[int, float | None]) -> dict[int, float]:
     return {k: total_amount / len(users) for k in users}
