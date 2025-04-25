0cac1b0e57a727b1a08530676a50d722011eba2b
PedroFlores1996 <pedro.flores.albuquerque@gmail.com>
2025-04-25 10:27:54 +0100

    add group overview view with page

--- app/group/views.py
+++ app/group/views.py
@@ -3,8 +3,39 @@ from flask_login import login_required, current_user
 
 from app.group import get_authorized_group
 from app.debt import get_group_user_debts
+from app.split.constants import OWED, PAYED, TOTAL
+
 
 bp = Blueprint("groups", __name__)
+
+
+@bp.route("/groups/<int:group_id>", methods=["GET"])
+@login_required
+def get_group_overview(group_id):
+    """
+    Displays the current user's total balance, debts, and recent expenses in the group.
+    """
+    if group := get_authorized_group(group_id):
+        # Get the current user's total balance in the group
+
+        # Get the current user's debts in the group
+        user_debts = get_group_user_debts([current_user], group.id)[current_user.id]
+        user_debts_ordered_by_amount = sorted(
+            user_debts[OWED] + user_debts[PAYED], key=lambda x: x.amount, reverse=True
+        )
+
+        # Get the 10 most recent expenses in the group
+        recent_expenses = group.expenses[:10]
+
+        return render_template(
+            "group/overview.html",
+            group=group,
+            user_group_balance=user_debts[TOTAL],
+            user_group_debts=user_debts_ordered_by_amount,
+            recent_expenses=recent_expenses,
+        )
+    else:
+        return jsonify({"error": "Group not found or access denied"}), 404
 
 
 @bp.route("/groups/<int:group_id>/users", methods=["GET"])
