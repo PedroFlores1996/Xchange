2e4e3df6fa6c3fb57a87072db22fbb302cb85233
PedroFlores1996 <pedro.flores.albuquerque@gmail.com>
2025-07-15 17:04:25 +0100

    repurposed index route to redirect to dashboard if user is authenticated

--- app/index/views.py
+++ app/index/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, url_for
 from flask_login import login_required
 
 bp = Blueprint("index", __name__)
@@ -7,10 +7,4 @@ bp = Blueprint("index", __name__)
 @bp.route("/")
 @login_required
 def home_page() -> str:
-    return render_template("index/index.html")
-
-
-@bp.route("/home")
-@login_required
-def home() -> str:
-    return render_template("index/home.html")
+    return redirect(url_for("user.user_dashboard"))
